cad0f479ba564c2d7fd89a75691276ac6d02cfc2
Anders Olme <anders.olme@gmail.com>
2017-01-17 18:02:16 +0100

    just some more lint fixes

--- gw2util.go
+++ gw2util.go
@@ -1,231 +1,236 @@
 package gw2util
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "strconv"
-    "strings"
-    "time"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"time"
 
-    "github.com/Jeffail/gabs"
+	"github.com/Jeffail/gabs"
 )
 
+// Gw2Api data used to access the json api.
 type Gw2Api struct {
-    BaseUrl string
-    Key     string
+	BaseUrl string
+	Key     string
 }
 
+// UserData conatins the connection between the chat name and an
+// gw2 account char name.
 type UserData struct {
-    GameId   string
-    ChatName string
-    Key      string
+	GameId   string
+	ChatName string
+	Key      string
 }
 
 type UserDataSlice []UserData
 
 type CharacterCache struct {
-    // need mutex here if called from goroutine..
-    charactersCache map[string]*gabs.Container
-    age             map[string]int64
+	// need mutex here if called from goroutine..
+	charactersCache map[string]*gabs.Container
+	age             map[string]int64
 }
 
 var cache = CharacterCache{charactersCache: make(map[string]*gabs.Container), age: make(map[string]int64)}
 
+// Update a users gw2 api key data or add the user to the slice if
+// the user hasnt set his data yet.
 func (b UserDataSlice) Update(user UserData) UserDataSlice {
-    found := false
-    for _, item := range b {
-        if item.ChatName == user.ChatName {
-            item = user
-            found = true
-        }
-    }
-    if !found {
-        b = append(b, user)
-    }
+	found := false
+	for _, item := range b {
+		if item.ChatName == user.ChatName {
+			item = user
+			found = true
+		}
+	}
+	if !found {
+		b = append(b, user)
+	}
 
-    return b
+	return b
 }
 
 func getCrafting(chars *gabs.Container, name string) []GW2Crafting {
-    size, _ := chars.ArrayCount("name")
-    var retVal = make([]GW2Crafting, 0)
+	size, _ := chars.ArrayCount("name")
+	var retVal = make([]GW2Crafting, 0)
 
-    for index := 0; index < size; index++ {
-        if strings.Contains(chars.Index(index).Search("name").String(), name) {
-            crafts := chars.Index(index).Search("crafting")
-            discipline := crafts.Search("discipline")
-            rating := crafts.Search("rating")
-            active := crafts.Search("active")
-            amountD, _ := crafts.ArrayCount("discipline")
-            amountR, _ := crafts.ArrayCount("rating")
-            amountA, _ := crafts.ArrayCount("active")
+	for index := 0; index < size; index++ {
+		if strings.Contains(chars.Index(index).Search("name").String(), name) {
+			crafts := chars.Index(index).Search("crafting")
+			discipline := crafts.Search("discipline")
+			rating := crafts.Search("rating")
+			active := crafts.Search("active")
+			amountD, _ := crafts.ArrayCount("discipline")
+			amountR, _ := crafts.ArrayCount("rating")
+			amountA, _ := crafts.ArrayCount("active")
 
-            if (amountD != amountR) && (amountD != amountA) {
-                return retVal
-            }
-            for n := 0; n < amountD; n++ {
-                Discipline := discipline.Index(n).String()
-                Rating, _ := strconv.ParseInt(rating.Index(n).String(), 10, 64)
-                Active, _ := strconv.ParseBool(active.Index(n).String())
-                retVal = append(retVal, GW2Crafting{Discipline, Rating, Active})
-            }
-        }
-    }
+			if (amountD != amountR) && (amountD != amountA) {
+				return retVal
+			}
+			for n := 0; n < amountD; n++ {
+				Discipline := discipline.Index(n).String()
+				Rating, _ := strconv.ParseInt(rating.Index(n).String(), 10, 64)
+				Active, _ := strconv.ParseBool(active.Index(n).String())
+				retVal = append(retVal, GW2Crafting{Discipline, Rating, Active})
+			}
+		}
+	}
 
-    return retVal
+	return retVal
 }
 
 func getCacheCaractersStruct(gw2 Gw2Api) *gabs.Container {
-    var jsonParsed *gabs.Container
+	var jsonParsed *gabs.Container
 
-    value, ok := cache.age[gw2.Key]
-    if (value+30 > time.Now().Unix()) && ok {
-        fmt.Println("using cached values")
-        jsonParsed = cache.charactersCache[gw2.Key]
-    } else {
-        fmt.Println("getting new values")
-        body := QueryAnetAuth(gw2, "characters")
-        jsonParsed, _ = gabs.ParseJSON(body)
-        cache.charactersCache[gw2.Key] = jsonParsed
-        cache.age[gw2.Key] = time.Now().Unix()
-    }
+	value, ok := cache.age[gw2.Key]
+	if (value + 30 > time.Now().Unix()) && ok {
+		fmt.Println("using cached values")
+		jsonParsed = cache.charactersCache[gw2.Key]
+	} else {
+		fmt.Println("getting new values")
+		body := QueryAnetAuth(gw2, "characters")
+		jsonParsed, _ = gabs.ParseJSON(body)
+		cache.charactersCache[gw2.Key] = jsonParsed
+		cache.age[gw2.Key] = time.Now().Unix()
+	}
 
-    return jsonParsed
+	return jsonParsed
 }
 
 func GetCrafting(gw2 Gw2Api, name string) []GW2Crafting {
-    jsonParsed := getCacheCaractersStruct(gw2)
+	jsonParsed := getCacheCaractersStruct(gw2)
 
-    return getCrafting(jsonParsed, name)
+	return getCrafting(jsonParsed, name)
 }
 
 func FindItem(gw2 Gw2Api, charName string, item string) []*GW2Item {
-    
-    jsonParsed := getCacheCaractersStruct(gw2)
-    items := GetItems(gw2, GetItemIdsFromBags(jsonParsed, charName))
-    return findItem(items, item)
+
+	jsonParsed := getCacheCaractersStruct(gw2)
+	items := GetItems(gw2, GetItemIdsFromBags(jsonParsed, charName))
+	return findItem(items, item)
 }
 
 // Tries to find a guild wars 2 item from item name or item detail.type
 func findItem(itemArr *gabs.Container, itemName string) []*GW2Item {
-    var retVal []*GW2Item
-    items, _ := itemArr.Children()
+	var retVal []*GW2Item
+	items, _ := itemArr.Children()
 
-    for _, item := range items {
-        if strings.Contains(strings.ToLower(item.Search("name").String()), strings.ToLower(itemName)) ||
-            strings.Contains(strings.ToLower(item.Path("details.type").String()), strings.ToLower(itemName)) {
-            //fmt.Println(item.StringIndent("", "\t"))
-            var gwItem GW2Item
-            dec := json.NewDecoder(strings.NewReader(item.String()))
-            dec.UseNumber()
-            err := dec.Decode(&gwItem)
-            if err != nil {
-                fmt.Println("Error during conversion ", err)
-            } else {
-                retVal = append(retVal, &gwItem)
-            }
-        }
-    }
-    return retVal
+	for _, item := range items {
+		if strings.Contains(strings.ToLower(item.Search("name").String()), strings.ToLower(itemName)) ||
+			strings.Contains(strings.ToLower(item.Path("details.type").String()), strings.ToLower(itemName)) {
+			//fmt.Println(item.StringIndent("", "\t"))
+			var gwItem GW2Item
+			dec := json.NewDecoder(strings.NewReader(item.String()))
+			dec.UseNumber()
+			err := dec.Decode(&gwItem)
+			if err != nil {
+				fmt.Println("Error during conversion ", err)
+			} else {
+				retVal = append(retVal, &gwItem)
+			}
+		}
+	}
+	return retVal
 }
 
 // Query the guild wars 2 json api for the items thsi character has in its bags.
 func GetItems(gw2 Gw2Api, Ids []uint64) *gabs.Container {
-    strIds := arrayToString(Ids, ",")
-    body := QueryAnet(gw2, "items", "ids", strIds)
-    jsonParsed, _ := gabs.ParseJSON(body)
-    return jsonParsed
+	strIds := arrayToString(Ids, ",")
+	body := QueryAnet(gw2, "items", "ids", strIds)
+	jsonParsed, _ := gabs.ParseJSON(body)
+	return jsonParsed
 }
 
 // Extract all items from the json blob
 func GetItemIdsFromBags(chars *gabs.Container, charName string) []uint64 {
-    var retVal []uint64
-    //fmt.Println(charName)
-    children, _ := chars.Children()
-    for _, char := range children {
-        if strings.Contains(strings.ToLower(char.S("name").String()), strings.ToLower(charName)) {
-            items := char.Path("bags.inventory.id")
-            //fmt.Println(items)
-            retVal = append(retVal, flattenIdArray(items)...)
-            //fmt.Println((retVal))
-        }
-    }
-    return retVal
+	var retVal []uint64
+	//fmt.Println(charName)
+	children, _ := chars.Children()
+	for _, char := range children {
+		if strings.Contains(strings.ToLower(char.S("name").String()), strings.ToLower(charName)) {
+			items := char.Path("bags.inventory.id")
+			//fmt.Println(items)
+			retVal = append(retVal, flattenIdArray(items)...)
+			//fmt.Println((retVal))
+		}
+	}
+	return retVal
 }
 
 func getCharacterNames(chars *gabs.Container) []string {
-    var retVal []string
-    children, _ := chars.Search("name").Children()
+	var retVal []string
+	children, _ := chars.Search("name").Children()
 
-    for _, char := range children {
-        retVal = append(retVal, strings.Trim(char.String(), "\""))
-    }
-    return retVal
+	for _, char := range children {
+		retVal = append(retVal, strings.Trim(char.String(), "\""))
+	}
+	return retVal
 }
 
 func GetCharacterNames(gw2 Gw2Api) []string {
-    body := QueryAnetAuth(gw2, "characters")
-    jsonParsed, _ := gabs.ParseJSON(body)
-    return getCharacterNames(jsonParsed)
+	body := QueryAnetAuth(gw2, "characters")
+	jsonParsed, _ := gabs.ParseJSON(body)
+	return getCharacterNames(jsonParsed)
 }
 
 func ReadUserData(filename string) UserDataSlice {
-    var retVal UserDataSlice
+	var retVal UserDataSlice
 
-    buff, err := ioutil.ReadFile(filename)
-    if err != nil {
-        fmt.Print(err)
-        return nil
-    }
-    err = json.Unmarshal(buff, &retVal)
-    if err != nil {
-        fmt.Print(err)
-        return nil
-    }
+	buff, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Print(err)
+		return nil
+	}
+	err = json.Unmarshal(buff, &retVal)
+	if err != nil {
+		fmt.Print(err)
+		return nil
+	}
 
-    return retVal
+	return retVal
 }
 
 func SaveUserData(userData UserDataSlice) string {
 
-    userJson, err := json.Marshal(userData)
-    if err != nil {
-        fmt.Print(err)
-        return "Can't marshall data"
-    }
-    err = ioutil.WriteFile("data.dat", userJson, 0600) // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-        return "Can't write to file"
-    }
-    return ""
+	userJson, err := json.Marshal(userData)
+	if err != nil {
+		fmt.Print(err)
+		return "Can't marshall data"
+	}
+	err = ioutil.WriteFile("data.dat", userJson, 0600) // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+		return "Can't write to file"
+	}
+	return ""
 }
 
 func GetUserData(users UserDataSlice, chatName string) UserData {
-    for _, user := range users {
-        if user.ChatName == chatName {
-            return user
-        }
-    }
+	for _, user := range users {
+		if user.ChatName == chatName {
+			return user
+		}
+	}
 
-    return UserData{"", "", ""}
+	return UserData{"", "", ""}
 }
 
 func UpsertUserData(users UserDataSlice, user UserData) UserDataSlice {
-    found := false
-    for index, item := range users {
-        if item.ChatName == user.ChatName {
-            users[index] = user
-            found = true
-            fmt.Println("found")
-        }
-    }
-    if !found {
-        users = append(users, user)
-    }
+	found := false
+	for index, item := range users {
+		if item.ChatName == user.ChatName {
+			users[index] = user
+			found = true
+			fmt.Println("found")
+		}
+	}
+	if !found {
+		users = append(users, user)
+	}
 
-    return users
+	return users
 }
 
 /*
--- util.go
+++ util.go
@@ -1,120 +1,120 @@
 package gw2util
 
 import (
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
-    "github.com/Jeffail/gabs"
+	"github.com/Jeffail/gabs"
 )
 
 type GW2Crafting struct {
-    Discipline string
-    Rating     int64
-    Active     bool
+	Discipline string
+	Rating     int64
+	Active     bool
 }
 
 type GW2Item struct {
-    ChatLink string `json:"chat_link"`
-    Details  struct {
-        ApplyCount  int     `json:"apply_count"`
-        Description string  `json:"description"`
-        DurationMs  float64 `json:"duration_ms"`
-        Icon        string  `json:"icon"`
-        Name        string  `json:"name"`
-        Type        string  `json:"type"`
-    } `json:"details"`
-    Flags        []string `json:"flags"`
-    GameTypes    []string `json:"game_types"`
-    Icon         string   `json:"icon"`
-    ID           int      `json:"id"`
-    Level        int      `json:"level"`
-    Name         string   `json:"name"`
-    Rarity       string   `json:"rarity"`
-    Restrictions []string `json:"restrictions"`
-    Type         string   `json:"type"`
-    VendorValue  int      `json:"vendor_value"`
+	ChatLink     string `json:"chat_link"`
+	Details      struct {
+			     ApplyCount  int     `json:"apply_count"`
+			     Description string  `json:"description"`
+			     DurationMs  float64 `json:"duration_ms"`
+			     Icon        string  `json:"icon"`
+			     Name        string  `json:"name"`
+			     Type        string  `json:"type"`
+		     } `json:"details"`
+	Flags        []string `json:"flags"`
+	GameTypes    []string `json:"game_types"`
+	Icon         string   `json:"icon"`
+	ID           int      `json:"id"`
+	Level        int      `json:"level"`
+	Name         string   `json:"name"`
+	Rarity       string   `json:"rarity"`
+	Restrictions []string `json:"restrictions"`
+	Type         string   `json:"type"`
+	VendorValue  int      `json:"vendor_value"`
 }
 
 func (b GW2Crafting) String() string {
-    return fmt.Sprintf("\nDiscipline: %s\nRating %d \nActive %t\n", b.Discipline, b.Rating, b.Active)
+	return fmt.Sprintf("\nDiscipline: %s\nRating %d \nActive %t\n", b.Discipline, b.Rating, b.Active)
 }
 
 func (b GW2Item) String() string {
-    return fmt.Sprintf("\n Name: %s\n Type: %s \nDetail.Name: %s\n", b.Name, b.Type, b.Details.Name)
+	return fmt.Sprintf("\n Name: %s\n Type: %s \nUrl: %s\n", b.Name, b.Type, b.ChatLink)
 }
 func GetKey(filename string) string {
-    buff, err := ioutil.ReadFile(filename) // just pass the file name
-    if err != nil {
-        fmt.Print(err)
-        return ""
-    }
+	buff, err := ioutil.ReadFile(filename) // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+		return ""
+	}
 
-    return strings.Trim(string(buff), "\n ")
+	return strings.Trim(string(buff), "\n ")
 }
 
 func arrayToString(a []uint64, delim string) string {
-    return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
+	return strings.Trim(strings.Replace(fmt.Sprint(a), " ", delim, -1), "[]")
 }
 
 func flattenIdArray(objectOfIdArrays *gabs.Container) []uint64 {
-    var (
-        retVal []uint64
-        tmpArr []uint64
-    )
+	var (
+		retVal []uint64
+		tmpArr []uint64
+	)
 
-    arrayOfIDArrays, _ := objectOfIdArrays.Children()
+	arrayOfIDArrays, _ := objectOfIdArrays.Children()
 
-    for _, IDArray := range arrayOfIDArrays {
-        Ids, _ := IDArray.Children()
-        length := len(IDArray.Data().([]interface{}))
-        tmpArr = make([]uint64, length)
-        for index, item := range Ids {
-            tmpArr[index] = uint64(item.Data().(float64))
-        }
-        retVal = append(retVal, tmpArr...)
-    }
-    return retVal
+	for _, IDArray := range arrayOfIDArrays {
+		Ids, _ := IDArray.Children()
+		length := len(IDArray.Data().([]interface{}))
+		tmpArr = make([]uint64, length)
+		for index, item := range Ids {
+			tmpArr[index] = uint64(item.Data().(float64))
+		}
+		retVal = append(retVal, tmpArr...)
+	}
+	return retVal
 }
 
 func doRestQuery(Url string) []byte {
-    tr := &http.Transport{
-        DisableCompression: true,
-    }
+	tr := &http.Transport{
+		DisableCompression: true,
+	}
 
-    req, err := http.NewRequest("GET", Url, nil)
-    if err != nil {
-        log.Fatal("NewRequest: ", err)
-        return nil
-    }
+	req, err := http.NewRequest("GET", Url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return nil
+	}
 
-    client := &http.Client{Transport: tr}
+	client := &http.Client{Transport: tr}
 
-    resp, err := client.Do(req)
-    if err != nil {
-        log.Fatal("Do: ", err)
-        return nil
-    }
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return nil
+	}
 
-    // Callers should close resp.Body
-    // when done reading from it
-    // Defer the closing of the body
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
+	// Callers should close resp.Body
+	// when done reading from it
+	// Defer the closing of the body
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 
-    return body
+	return body
 }
 
 func QueryAnet(gw2 Gw2Api, command string, paramName string, paramValue string) []byte {
-    Url := fmt.Sprintf("%s%s%s%s%s%s", gw2.BaseUrl, command, "?", paramName, "=", paramValue)
-    //fmt.Println("Url: ", Url)
-    return doRestQuery(Url)
+	Url := fmt.Sprintf("%s%s%s%s%s%s", gw2.BaseUrl, command, "?", paramName, "=", paramValue)
+	//fmt.Println("Url: ", Url)
+	return doRestQuery(Url)
 }
 
 func QueryAnetAuth(gw2 Gw2Api, command string) []byte {
-    Url := fmt.Sprintf("%s%s%s%s%s", gw2.BaseUrl, command, "?access_token=", gw2.Key, "&page=0")
+	Url := fmt.Sprintf("%s%s%s%s%s", gw2.BaseUrl, command, "?access_token=", gw2.Key, "&page=0")
 
-    return doRestQuery(Url)
+	return doRestQuery(Url)
 }
